ae323579b8c26b5721390d9a27ea309edabf838f
Koop the Koopa <61279093+KoopintheHub@users.noreply.github.com>
2023-04-06 22:08:38 +0100

    Add files via upload

--- webassets/main.js
+++ webassets/main.js
@@ -1,7 +1,12 @@
+// copyright 2021 - 202X
 document.addEventListener('contextmenu', event => event.preventDefault());
 
 function enableJs() {
 	document.getElementById('nojs').style.visibility = "hidden";
+}
+
+function downloadModel(name) {
+	window.location.href = 'mdlDATA/sm64' + name + '.zip';
 }
 
 function bootWeb() {
